bb5aa521ac98e7d4ee1cf6d70d69988388a0e93e
dev-bitbulk <dosen@gorillajobs.com.au>
2022-07-19 17:04:20 +1000

    components update

---
+++ Frontend/frontend/src/stories/StudentCard.stories.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import StudentCard from './StudentCard';
+
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Example/StudentCard',
+  component: StudentCard,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof StudentCard>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof StudentCard> = (args) => (<StudentCard {...args} />);
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  imageUrl: "",
+  userName:"Charlie",
+  courseName:"COMP9323"
+};
---
+++ Frontend/frontend/src/stories/StudentCard.tsx
@@ -0,0 +1,48 @@
+import * as React from "react";
+import { useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import SkipNextIcon from "@mui/icons-material/SkipNext";
+import { Avatar, CardActionArea } from "@mui/material";
+
+export interface IPostingCardProps {
+  imageUrl?: string;
+  userName?: string;
+  courseName?: string;
+}
+
+export default function PostingCard(props: IPostingCardProps) {
+  return (
+    <Box sx={{ width: 380, height: 462 }}>
+      <Card sx={{ width: 380, height: 406, borderRadius: 5, marginTop: 7 }}>
+        <CardActionArea>
+          <CardContent>
+            <Avatar
+              sx={{
+                width: 260,
+                height: 260,
+                borderRadius: 5,
+                marginTop: 0,
+                mx: "auto",
+              }}
+              variant="rounded"
+              src={props.imageUrl}
+            />
+          </CardContent>
+          <Typography variant="body1" component="h2" sx={{mx:"auto"}}>
+            {props.userName}
+          </Typography>
+          <Typography variant="body1" component="h2">
+            {props.courseName}
+          </Typography>
+        </CardActionArea>
+      </Card>
+    </Box>
+  );
+}
