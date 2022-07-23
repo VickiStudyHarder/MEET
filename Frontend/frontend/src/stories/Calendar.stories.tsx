5aea28e60706bff5534030bd29a26ec0149d0aca
quxianyi <90333869+dosen-gorillajobs@users.noreply.github.com>
2022-07-23 15:45:54 +0800

    feat：Calendar

--- Frontend/frontend/src/stories/Calendar.stories.tsx
+++ Frontend/frontend/src/stories/Calendar.stories.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import FullCalendar from './FullCalendar';
+import Calendar from './Calendar';
 
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Example/FullCalendar',
-  component: FullCalendar,
+  title: 'Example/Calendar',
+  component: Calendar,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
     backgroundColor: { control: 'color' },
   },
-} as ComponentMeta<typeof FullCalendar>;
+} as ComponentMeta<typeof Calendar>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof FullCalendar> = (args) => (<FullCalendar {...args}/>);
+const Template: ComponentStory<typeof Calendar> = (args) => (<Calendar {...args}/>);
 
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   primary: true,
-  label: 'FullCalendar',
+  label: 'Calendar',
 };
