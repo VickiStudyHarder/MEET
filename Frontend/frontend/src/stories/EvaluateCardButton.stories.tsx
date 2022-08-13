81f322e0b816a4982e2a7cfa651e2ba8429cfb2a
spyxgr <107198839+spyxgr@users.noreply.github.com>
2022-08-13 20:15:32 +1000

    evaluateCard + new Rating page

--- Frontend/frontend/src/stories/EvaluateCardButton.stories.tsx
+++ Frontend/frontend/src/stories/EvaluateCardButton.stories.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import RatingCardMentor from './RatingCardMentor';
+import EvaluateCardButton from './EvaluateCardButton';
 
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'RatingCardMentor',
-  component: RatingCardMentor,
+  component: EvaluateCardButton,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
     backgroundColor: { control: 'color' },
   },
-} as ComponentMeta<typeof RatingCardMentor>;
+} as ComponentMeta<typeof EvaluateCardButton>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof RatingCardMentor> = (args) => (<RatingCardMentor {...args} />);
+const Template: ComponentStory<typeof EvaluateCardButton> = (args) => (<EvaluateCardButton {...args} />);
 
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
