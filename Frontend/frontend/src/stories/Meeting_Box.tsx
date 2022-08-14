090bce4ebd2b9024580bafd59020754e2e412202
Cavinash C <42435186+CChelliah@users.noreply.github.com>
2022-08-14 21:18:21 +1000

    update dashboard

--- Frontend/frontend/src/stories/Meeting_Box.tsx
+++ Frontend/frontend/src/stories/Meeting_Box.tsx
@@ -1,102 +1,114 @@
-import * as React from "react";
+import * as React from 'react';
 
-
-import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
-import { Avatar, CardActionArea, Icon } from "@mui/material";
-import Grid from "@mui/material/Grid";
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { Avatar, CardActionArea, Icon } from '@mui/material';
+import Grid from '@mui/material/Grid';
 
 import AssignmentIcon from '@mui/icons-material/Assignment';
-import { green, pink } from '@mui/material/colors';
+import NotesIcon from '@mui/icons-material/Notes';
+import { green, pink, blue } from '@mui/material/colors';
 import GroupsIcon from '@mui/icons-material/Groups';
 
-
-
-
-export interface Meeting_Box {
-  Box_name?: string;
-  Meeting_name1?: string;
-  Meeting_name2?: string;
-
+export interface IMeetingBox {
+  boxName: 'To Do' | 'Meeting Note';
+  meetingName1: string;
+  meetingName2: string;
 }
 
-
-export default function Meeting_Box(props: Meeting_Box) {
-    return (
-        <Box sx={{ width: 260, height: 210 }}>
-        <Card sx={{ width: 260, height: 210, borderRadius: 5}}>
-            <CardActionArea>
-                <CardContent>
-                    <Grid container 
-                        direction = "row" 
-                        sx={{ 
-                            width: 260, 
-                            height: 210, 
-                            }}>
-                        
-                        <Grid container item 
-                            direction = "column" 
-                            sx={{
-                                width: 260, 
-                                height: 20, 
-                                }}>
-
-                            <Avatar sx={{ bgcolor: pink[500] }}>
-                                    <AssignmentIcon />
-                                  </Avatar>
-                            
-                        </Grid>
-                        
-                        <Grid container item sx={{mx:"auto"}}>
-                                <Typography 
-                                    variant="body1" 
-                                    component="h1" 
-                                    sx={{ 
-                                        fontSize : "1.5rem", 
-                                        fontWeight: 600
-                                        }}>
-                                {props.Box_name}
-                                </Typography>
-                        </Grid>
-
-
-                        <Grid container item>
-                        <Typography 
-                            variant="body1" 
-                            component="h2" 
-                            sx={{
-                                width: 245, 
-                                color:"#70798B", 
-                                fontSize: 14
-                                }}>
-                        <GroupsIcon/>
-                        {props.Meeting_name1}
-                        </Typography>
-                        </Grid>
-
-                        <Grid container item>
-                        <Typography 
-                            variant="body1" 
-                            component="h2" 
-                            sx={{
-                                width: 245, 
-                                color:"#70798B", 
-                                marginTop: 0.1, 
-                                fontSize: 14
-                                }}>
-                        <GroupsIcon/>
-                        {props.Meeting_name2}
-                        </Typography>
-                        </Grid>
-
-                        
-                    </Grid>
-                            
-                </CardContent>
-            </CardActionArea>
-        </Card>
+export default function MeetingBox(props: IMeetingBox) {
+  const dueDate =
+    props.boxName === 'To Do'
+      ? new Date(props.meetingName1).toLocaleString('en-AU', {
+          year: 'numeric',
+          month: '2-digit',
+          day: '2-digit',
+          hour: '2-digit',
+          minute: '2-digit',
+          second: '2-digit',
+        })
+      : props.meetingName1;
+  return (
+    <Box sx={{ width: 260, height: 210, m: 2 }}>
+      <Card sx={{ width: 260, height: 210, borderRadius: 5 }}>
+        <CardActionArea>
+          <CardContent sx={{ pt: 4 }}>
+            <Box sx={{ display: 'flex', flexDirection: 'row', width: '100%', m:2 }}>
+              {props.boxName === 'To Do' ? (
+                <Avatar sx={{ bgcolor: blue[500], m: 'auto' }}>
+                  <NotesIcon />
+                </Avatar>
+              ) : (
+                <Avatar sx={{ bgcolor: pink[500], m: 'auto' }}>
+                  <AssignmentIcon />
+                </Avatar>
+              )}
+              <Typography
+                variant='body1'
+                component='h1'
+                align='center'
+                sx={{
+                  display: 'flex',
+                  flexGrow: 1,
+                  fontSize: '1.5rem',
+                  fontWeight: 600,
+                  m: 'auto',
+                  justifyContent: 'center',
+                }}
+              >
+                {props.boxName}
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                width: '100%',
+                m:2
+              }}
+            >
+              <GroupsIcon sx={{ mr: 2 }} />
+              <Typography
+                variant='body1'
+                component='h2'
+                sx={{
+                  width: 245,
+                  color: '#70798B',
+                  fontSize: 14,
+                  justifyContent: 'center',
+                }}
+              >
+                {dueDate}
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                width: '100%',
+                m:2
+              }}
+            >
+              <GroupsIcon sx={{ mr: 2 }} />
+              <Typography
+                variant='body1'
+                component='h2'
+                sx={{
+                  width: 245,
+                  color: '#70798B',
+                  fontSize: 14,
+                  m: 'auto',
+                  justifyContent: 'center',
+                }}
+              >
+                {props.meetingName2}
+              </Typography>
+            </Box>
+          </CardContent>
+        </CardActionArea>
+      </Card>
     </Box>
-    );
-  }
+  );
+}
