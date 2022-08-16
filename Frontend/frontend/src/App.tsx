748621dc422b613a4e2ee221d013ef29101e9b3b
Cavinash C <42435186+CChelliah@users.noreply.github.com>
2022-08-17 00:39:31 +1000

    re add protected route

--- Frontend/frontend/src/App.tsx
+++ Frontend/frontend/src/App.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import {
   Agenda,
   Calendar,
@@ -15,45 +15,44 @@ import {
   LandingPage,
   GroupChat,
   Meetings,
-  ToDoHome, 
-  MeetingDashboard
-} from "./components/pages";
-import GoogleAuth from "./components/organisms/Google/Google";
-import ProtectedRoute from "./hooks/ProtectedRoute";
-import API from "./components/pages/API";
-import { AppContextProvider } from "./contexts/AppContext";
-import "./assets/font/font.css";
+  ToDoHome,
+  MeetingDashboard,
+} from './components/pages';
+import GoogleAuth from './components/organisms/Google/Google';
+import ProtectedRoute from './hooks/ProtectedRoute';
+import API from './components/pages/API';
+import { AppContextProvider } from './contexts/AppContext';
+import './assets/font/font.css';
 
 function App() {
   return (
-      <Router>
-        <div style={{ height: "100%", width: "100%" }}>
-          <AppContextProvider>
-            <Routes>
-              {/* <Route path='/' element={<ProtectedRoute />}> */}
-              <Route path="/home" element={<Home />} />
-              {/* </Route> */}
-              <Route path="/signup" element={<SignUp />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<SignUp />} />
-              <Route path="/agenda/:id" element={<Agenda />} />
-              <Route path="/calendar" element={<Calendar />} />
-              <Route path="/group" element={<Group />} />
-              <Route path="/group/:id" element={<GroupChat />} />
-              <Route path="/meeting/:id" element={<MeetingDashboard />} />
-              <Route path="/meetings" element={<Meetings />} />
-              <Route path="/todo" element={<ToDos />} />
-              <Route path="/todo/:id" element={<ToDoHome />} />
-              <Route path="/notes/:id" element={<Notes />} />
-              <Route path="/rating" element={<Rating />} />
-              <Route path="/recording/:id" element={<Recording />} />
-              <Route path="/todos" element={<ToDos />} />
-              <Route path="/login1" element={<LandingPage />} />
-              <Route path="/api" element={<API />} />
-            </Routes>
-          </AppContextProvider>
-        </div>
-      </Router>
+    <Router>
+      <div style={{ height: '100%', width: '100%' }}>
+        <AppContextProvider>
+          <Routes>
+            <Route path='/' element={<ProtectedRoute />}>
+              <Route path='/home' element={<Home />} />
+              <Route path='/agenda/:id' element={<Agenda />} />
+              <Route path='/calendar' element={<Calendar />} />
+              <Route path='/group' element={<Group />} />
+              <Route path='/group/:id' element={<GroupChat />} />
+              <Route path='/meeting/:id' element={<MeetingDashboard />} />
+              <Route path='/meetings' element={<Meetings />} />
+              <Route path='/todo' element={<ToDos />} />
+              <Route path='/todo/:id' element={<ToDoHome />} />
+              <Route path='/notes/:id' element={<Notes />} />
+              <Route path='/rating' element={<Rating />} />
+              <Route path='/recording/:id' element={<Recording />} />
+              <Route path='/todos' element={<ToDos />} />
+              <Route path='/login1' element={<LandingPage />} />
+              <Route path='/api' element={<API />} />
+            </Route>
+            <Route path='/signup' element={<SignUp />} />
+            <Route path='/login' element={<Login />} />
+          </Routes>
+        </AppContextProvider>
+      </div>
+    </Router>
   );
 }
 
